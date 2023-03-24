7e10d6eafb7166c5fe9a8914de31bb6379e55212
lahmbert <muhammadsabran86@gmail.com>
2023-03-24 23:26:17 +0000

    add new

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,78 @@
+import React from 'react'
+
+import Logo from '../../assets/images/logo512.png'
+import { Container } from 'reactstrap'
+import { NavLink, Link } from 'react-router-dom'
+
+import '../../styles/header.css'
+
+const nav__link = [
+  {
+    display: 'Home',
+    path: '/home'
+
+  },
+  {
+    display: 'Foods',
+    path: '/foods'
+
+  },
+  {
+    display: 'Cart',
+    path: '/cart'
+
+  },
+  {
+    display: 'Contact',
+    path: '/contact'
+
+  },
+]
+
+const Header = () => {
+
+  
+  return <header className='header'>
+    <Container>
+      <div className="nav__wrapper d-flex align-items-center justify-content-between">
+        <div className="logo">
+          <img src={Logo} alt="logo" className='w-25' />
+          <h5>Warung <span>Adifa</span></h5>
+        </div>
+
+        {/*====== Menu ====== */}
+        <div className="navigation">
+          <div className="menu d-flex align-items-center gap-5">
+            {
+              nav__link.map((item,index) => (
+                <NavLink to={item.path} key={index} className={navClass => navClass.isActive ? 'active__menu' : ''} >
+                  {item.display}
+                </NavLink>
+              ))
+            }
+          </div>
+        </div>
+
+        {/*======= Right Icon ========= */}
+        <div className="nav__right d-flex align-items-center gap-4">
+          <span className="cart__icon">
+          <i className="ri-shopping-bag-line"></i>
+          <span className="cart__badge">2</span>
+          </span>
+
+          <span className="user">
+            <Link to='/login'><i className="ri-user-3-line"></i></Link>
+          </span>
+
+          <span className="mobile__menu">
+          <i className="ri-menu-line"></i>
+          </span>
+        </div>
+
+
+      </div>
+    </Container>
+  </header>
+}
+
+export default Header
